21cdbebc40fdb35caf804cd9ad4e96196e712747
User <User@192.168.2.3>
2025-04-19 22:32:00 +0800

    Update Spring Boot練習範例_20250419(@RequestHeader, @PathVariable)

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,18 @@ public class MyController {
 	public String test2(@RequestBody Student student) {
 		System.out.println("student's id = " + student.getId());
 		System.out.println("student's name: " + student.getName());
+		return "請求成功";
+	}
+	
+	@RequestMapping("/test3")
+	public String test3(@RequestHeader String info) {
+		System.out.println("info = " + info);
+		return "請求成功";
+	}
+	
+	@RequestMapping("/test4/{id}")
+	public String test4(@PathVariable Integer id) {
+		System.out.println("id = " + id);
 		return "請求成功";
 	}
 }
